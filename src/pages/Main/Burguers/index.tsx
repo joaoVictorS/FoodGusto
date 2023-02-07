7bafd157436cdeb7b30d1b9cbcc4ea5ca93b01ba
joaoVictorS <jonnymcz2@gmail.com>
2023-02-07 14:09:45 -0300

    Atualização de consudo de dados da API nas pages drinks, icecreams, pizzas

--- src/pages/Main/Burguers/index.tsx
+++ src/pages/Main/Burguers/index.tsx
@@ -9,21 +9,19 @@ import { getBurguers } from "../../../services/api"
 export default function Burguers() {
   const [burgers, setBurguers] = useState([]);
 
-  useEffect(()=>{
-    ;(async () =>{
+  useEffect(() => {
+    ; (async () => {
       const burguerRequest = await getBurguers()
 
       setBurguers(burguerRequest.data)
     })()
   }, [])
 
-  console.log({burgers})
-
-  return(
-  <>
-    <Head title='Hamburguers' description="Nossos melhores burgueres" />
-    <SnackTitle> Hamburgueres</SnackTitle>
-    <Snacks snacks={burgers}/>
-  </>)
+  return (
+    <>
+      <Head title='Hamburguers' description="Nossos melhores burgueres" />
+      <SnackTitle> Hamburgueres</SnackTitle>
+      <Snacks snacks={burgers} />
+    </>)
 
 }
