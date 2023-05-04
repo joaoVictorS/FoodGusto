d7db483d7432e9972f67e81b74ad61d9f331b0a3
joaoVictorS <jonnymcz2@gmail.com>
2023-05-04 18:12:14 -0300

    add conditional rendering for mobile

---
+++ src/pages/MyCart/Table/TableMobile/index.tsx
@@ -0,0 +1,3 @@
+export default function TableMobile() {
+  return <h1>mobile</h1>
+}
--- src/pages/MyCart/Table/index.tsx
+++ src/pages/MyCart/Table/index.tsx
@@ -1,15 +1,26 @@
-import ConfirmOrder from '../../../components/ConfirmOrder'
+import { useState, useEffect } from 'react'
 import { useCart } from '../../../hooks/useCart'
 import TableDesktop from './TableDesktop'
+import TableMobile from './TableMobile'
 
 export default function Table() {
+  const [windowWidth, setWindowWidth] = useState(document.documentElement.clientWidth)
+
   const { cart } = useCart()
 
-  if (cart.length === 0) return <h1>Ops parece que voce não tem pedidos, peça já</h1>
-  return (
-    <>
-      <TableDesktop />
-      <ConfirmOrder />
-    </>
-  )
+  useEffect(() => {
+    function updateTableComponentBasedWindowWidth() {
+      const currentWidth = document.documentElement.clientWidth
+      setWindowWidth(currentWidth)
+    }
+    window.addEventListener('resize', updateTableComponentBasedWindowWidth)
+
+    return () => {
+      window.removeEventListener('resize', updateTableComponentBasedWindowWidth)
+    }
+  }, [windowWidth])
+
+  if (cart.length === 0) return <h1>Ops parece que voce não tem pedidos, peça já {windowWidth}</h1>
+
+  return windowWidth > 768 ? <TableDesktop /> : <TableMobile />
 }
